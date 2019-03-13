b24a9fad939550a59735b568e51779f61474fee4
Palevich <TBO9914@onetakeda.com>
2019-03-13 16:39:27 +0300

    Support for any lenght of tuple added

--- REMINDER_DATES.py
+++ REMINDER_DATES.py
@@ -1,8 +1,12 @@
-REMINDER_DATES = (5, 3, 2, 1, 0,)
-
-def message():
-	return 'Мы оповестим вас о предстоящих событиях за {}, {}, {}, {} и {} дней'.format(REMINDER_DATES[0], REMINDER_DATES[1], REMINDER_DATES[2],REMINDER_DATES[3],REMINDER_DATES[4])
-
+def CREATE_MESSAGE(dates):
+	message = 'Мы оповестим вас о предстоящих событиях за {} и {} дней'.format(str(dates[0:-1])[1:-1], str(dates[-1]))
+	return message
 
 if __name__ == '__main__':
-	print(message())
+	print("Examples:")
+
+	REMINDER_DATES = (5, 3, 2, 1, 0,)
+	print(CREATE_MESSAGE(REMINDER_DATES))
+
+	REMINDER_DATES = (10, 5, 3, 2, 1, 0,)
+	print(CREATE_MESSAGE(REMINDER_DATES))
