8eaa9b8d3df24b5b6e3ccd14c0d72c1f4c31d7fb
Palevich <TBO9914@onetakeda.com>
2019-03-14 18:18:55 +0300

    Add sample doctest, check input arguments

--- REMINDER_DATES.py
+++ REMINDER_DATES.py
@@ -1,47 +1,48 @@
-def create_message(dates, separator=None):
+"""
+This is an exercise maintained with my mentor Zcho05.
+The exercise contains function called create_message() that returns a message about some reminder.
 
-	# create pieces of final message
-	message1 = 'Мы оповестим вас о предстоящих событиях за '
-	message2 = ''
-	message3 = ''
+For example:
+>>> create_message((2, 4, 6, 7,))
+'Мы оповестим вам о предстоящих событиях за 2, 4, 6 и 7 дней'
 
-	#fill message2 with days in readable format
-	if len(dates) == 0:
+"""
+
+def create_message(dates, separator=', '):
+	#check input argument
+	if isinstance(dates, str):
+		try:
+			dates = list(map(int, dates.split(',')))
+		except ValueError:
+			raise Exception('Буквы вместо дат')
+	if not len(dates):
 		raise Exception('Отсутствуют даты')
-	elif len(dates) == 1:
-		message2 = str(dates)[1:-1]
-	else:
-		message2 = str(dates[:-1])[1:-1] + ' и ' + str(dates[-1])
+
+	# create final message
+
+	message = ''
+
+
+
+	# choose correct case for ending word
+	if str(dates).endswith('1', 0, -1):
+		message = ' день'
+	elif str(dates).endswith(('2', '3', '4'), 0, -1):
+		message = ' дня'
+	elif str(dates).endswith(('5', '6', '7', '8', '9', '0'), 0, -1):
+		message = ' дней'
+
+	message = 'Мы оповестим вас о предстоящих событиях за {}'.format(str(dates)[1:-1]) + message
 
 	# replace comma with an selected separator
 	if separator:
-		message2 = message2.replace(',', separator)
+		message = message.replace(', ', separator)
 
-	# choose correct case for ending word
-	if message2.endswith('1'):
-		message3 = ' день'
-	elif message2.endswith(('2', '3', '4')):
-		message3 = ' дня'
-	elif message2.endswith(('5', '6', '7', '8', '9', '0')):
-		message3 = ' дней'
-
-	return message1 + message2 + message3
+	return message
 
 if __name__ == '__main__':
-
-	print("Examples:")
-
-	REMINDER_DATES = '5, 3, 2, 1'
-	print(create_message(REMINDER_DATES))
-
-	REMINDER_DATES = (5, 3, 2, 1, 0,)
-	print(create_message(REMINDER_DATES, '/'))
-
-	REMINDER_DATES = (10, 5, 3, 4,)
-	print(create_message(REMINDER_DATES, separator=';'))
-
-	REMINDER_DATES = (10, 5, 3, 2, 1, 8)
-	print(create_message(REMINDER_DATES))
-
-	REMINDER_DATES = (10, 5, 3, 2, 1, 10)
-	print(create_message(REMINDER_DATES))
+	r = '4, 6, 3, 3,'
+	print(create_message(r))
+	#
+	# import doctest
+	# doctest.testmod()
