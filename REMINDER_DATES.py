c5465953688fac8ad1052b6b719e0a71a882a729
Palevich <TBO9914@onetakeda.com>
2019-03-15 17:09:14 +0300

    Intermediary commit, some features are broken

--- REMINDER_DATES.py
+++ REMINDER_DATES.py
@@ -9,16 +9,36 @@ For example:
 """
 
 def create_message(dates, separator=', '):
+	'''
+	This function construct message from dates:
+
+	>>>create_message((2, 4, 5, 7))
+	'Мы оповестим вас о предстоящих событиях за 2, 4, 5 и 7 дней'
+
+	You can even use a tuple with only date. Conjunction "и" ("and") will not be inputed then
+
+	>>>create_message((7,))
+	'Мы оповестим вас о предстоящих событиях за 7 дней'
+
+	Also strings and lists is supported:
+	>>>create_message((7,))
+	'Мы оповестим вас о предстоящих событиях за 7 дней'
+	'''
+
+
 	#check input argument
-	# # TODO convert string to list
-	# if isinstance(dates, str):
-	# 	try:
-	# 		dates = list(map(int, dates.split(',')))
-	# 	except ValueError:
-	# 		raise Exception('Буквы вместо дат')
+	if isinstance(dates, str):
+		try:
+			dates = list(map(int, dates.split()))
+		except ValueError:
+			raise Exception('Буквы вместо дат, либо неверная запятая')
 	if not len(dates):
 		raise Exception('Отсутствуют даты')
+	for n in dates:
+		if not type(n) == int:
+			raise Exception('Вместо даты использована буква')
 
+	message = ''
 	# choose correct case for ending word
 	if str(dates).endswith('1', 0, -1):
 		message = ' день'
@@ -28,7 +48,10 @@ def create_message(dates, separator=', '):
 		message = ' дней'
 
 	# construct final message
-	message = 'Мы оповестим вас о предстоящих событиях за {}'.format(str(dates)[1:-1]) + message
+	if len(dates) > 1:
+		message = 'Мы оповестим вас о предстоящих событиях за {} и {}'.format(str(dates[:-1])[1:-1], str(dates[-1])) + message
+	else:
+		message = 'Мы оповестим вас о предстоящих событиях за {}'.format(str(dates)[1:-1]) + message
 
 	# replace comma with an selected separator
 	message = message.replace(', ', separator)
@@ -36,7 +59,7 @@ def create_message(dates, separator=', '):
 	return message
 
 if __name__ == '__main__':
-	r = '4, 6, 3, 3'
+	r = '7, 8, 5, 7, '
 	print(create_message(r))
 	#
 	# import doctest
