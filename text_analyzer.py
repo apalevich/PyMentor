86ee59ebc678dcb516904100dcfdb85dd716c2bd
apalevich <artyompalevich@MacBook-Pro-Artyom.local>
2019-03-23 20:26:12 +0400

    Counter for words excluded in separated function and optimized

--- text_analyzer.py
+++ text_analyzer.py
@@ -16,10 +16,10 @@ text = """
 # заглавных букв
 
 '''
-Here is analyze tool for a text. It counts:
+Here is a few tools to analyze a text. Here is a full list of functions and their features:
 
-* words
-* sentences
+* count_words — counting words (more than 1 character)
+* count_sentences - c
 * words in russian
 * words in english
 * numbers
@@ -41,27 +41,60 @@ and it returns all the counters as variables and their integers:
 
 '''
 
-def text_analyzer(text):
+# first, we create variables using in some functions
 
-    #let's convert text into list, it reveals useful in a some counters
-    text_array = list(map(str, text.split()))
+rus_low = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+rus_big = rus_low.upper()
+eng_low = 'abcdefghijklmnopqrstuvwxyz'
+eng_big = eng_low.upper()
 
+text_array = list(map(str, text.split()))
+ending_characters = ['.', '!', '?']
+
+def count_words(text):
+    '''
+    Function counts words in a text.
+    Numbers and words less than 2 characters doesn't counts:
+
+    >>> count_words('Please buy one cucumber & 3 apples')
+    5
+    '''
     # count words
     words = 0
     for i in text_array:
         if i[0].isalpha() and len(i) > 1:
             words += 1
+    return words
 
-    # count sentences
+def count_sentences(text):
+    '''
+    description
+    '''
     sentences = 0
-    for c in text:
-        if c == '.':
+    sentence_beginning = 0
+    sentence_ending = 0
+
+    for i in text_array:
+        if i[0] in eng_big or i[0] in rus_big:
+            sentence_beginning += 1
+
+        elif i[-1] in ending_characters:
+            sentence_ending += 1
+
+        if sentence_beginning > 0 and sentence_ending > 0:
+            sentence_beginning, sentence_ending = 0, 0
             sentences += 1
-    # fix cases when last sentence doesn't ends with a dot
-    if text[-1] != '.':
-        sentence += 1
+        print('{}, sentence_beginning: {}, sentence_ending: {}, total: {}'.format(i, sentence_beginning, sentence_ending, sentences))
+
+    return sentences
+
+    # for c in text:
+    #     if c == '.':
+    #         sentences += 1
+
     # TODO: придумать, как исключить подсчёт точек в сокращениях и числах
 
+def temp(text):
     # count russian words
     russians = 0
     # for i in text_array:
@@ -94,7 +127,10 @@ def text_analyzer(text):
 
 if __name__ == "__main__":
 
-    text_analyzer(text)
+    text_array = list(map(str, text.split()))
+    print(text_array)
+
+    print(count_sentences(text))
 
     # import doctest
     # doctest.testmod()
