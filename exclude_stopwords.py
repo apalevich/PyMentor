310c77944d6c68462bcc38966fcaa3e46e50e26a
apalevich <artyompalevich@MacBook-Pro-Artyom.local>
2019-03-29 19:39:46 +0400

    Function returns dictionary with frequency of using every word in a text

--- exclude_stopwords.py
+++ exclude_stopwords.py
@@ -5,9 +5,12 @@
 import requests
 
 def exclude_stopwords(path_to_text):
+    # TODO: написать доктест
+
     # get input text from a file to list
     original_text = open(path_to_text, 'r')
-    text_list = original_text.read().split()
+    text_string = original_text.read()
+    text_list = text_string.split()
 
     # download file with stop-words
     url = 'https://github.com/Alir3z4/stop-words/raw/bd8cc1434faeb3449735ed570a4a392ab5d35291/russian.txt'
@@ -28,10 +31,28 @@ def exclude_stopwords(path_to_text):
         if not word in stopwords_list:
             cleaned_text.append(word)
 
-    return ' '.join(cleaned_text)
+    #collect_statistics
+    stats = {}
+    for element in cleaned_text:
+        keyword = ''
+
+        for char in element:
+            if char.isalpha():
+                keyword += char
+
+        if keyword in stats.keys():
+            stats[keyword] += 1
+        else:
+            stats[keyword] = 1
+
+    return stats
+
+
+# TODO:
+# слова в виде листа
+# считать сколько раз входит
 
 if __name__ == "__main__":
-    print(exclude_stopwords('/tmp/text.txt'))
-
+    print(exclude_stopwords('/tmp/copy.txt'))
 
 # TODO: выдавать статистику по частоте вхождения в текст каждого слова - самые частые слова сначала.
