993e4748a1448cbe3e6b7447725157a55032d08a
Palevich <TBO9914@onetakeda.com>
2019-04-11 18:41:29 +0300

    Add comments, style improved with PEP 8

--- counter.py
+++ counter.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-import re, collections
+import collections
 
 def counter(text):
     """
@@ -17,6 +16,8 @@ def counter(text):
     >>> counter('./sample.txt')
     'и - 6 раз, ты - 3 раз, друг - 2 раз, на - 2 раз, сквозь - 2 раз, в - 2 раз, мороз - 1 раз, день - 1 раз, еще - 1 раз, прелестный - 1 раз, открой - 1 раз, сомкнуты - 1 раз, негой - 1 раз, взоры - 1 раз, навстречу - 1 раз, северной - 1 раз, звездою - 1 раз, севера - 1 раз, вьюга - 1 раз, мутном - 1 раз, небе - 1 раз, мгла - 1 раз, как - 1 раз, бледное - 1 раз, тучи - 1 раз, мрачные - 1 раз, печальная - 1 раз, сидела - 1 раз, а - 1 раз, погляди - 1 раз, под - 1 раз, голубыми - 1 раз, небесами - 1 раз, великолепными - 1 раз, блестя - 1 раз, снег - 1 раз, прозрачный - 1 раз, лес - 1 раз, один - 1 раз, ель - 1 раз, иней - 1 раз, речка - 1 раз, подо - 1 раз, льдом - 1 раз, вся - 1 раз, комната - 1 раз, янтарным - 1 раз, блеском - 1 раз, веселым - 1 раз, треском - 1 раз, трещит - 1 раз, затопленная - 1 раз, приятно - 1 раз, думать - 1 раз, у - 1 раз, но - 1 раз, не - 1 раз, велеть - 1 раз, ли - 1 раз, санки - 1 раз, кобылку - 1 раз, бурую - 1 раз, скользя - 1 раз, по - 1 раз, утреннему - 1 раз, предадимся - 1 раз, бегу - 1 раз, нетерпеливого - 1 раз, коня - 1 раз, навестим - 1 раз, поля - 1 раз, недавно - 1 раз, столь - 1 раз, милый - 1 раз, для - 1 раз'
     """
+
+    # Get original text from a file or string
     try:
         with open(text, 'r', encoding = 'utf-8') as original_text:
             text_string = original_text.read()
@@ -25,13 +26,19 @@ def counter(text):
         text_string = text
         text_list = text_string.lower().split()
 
+    # Prepare string for function's result
     answer = ''
 
+    # Sort words by quantity
     c = collections.Counter(text_list).most_common()
 
+    # Add sorted words to result string in a human-readable format
     for i in list(c):
         if i[0].isalpha():
-            answer += '{} - {} раз, '.format(i[0], i[1])
+            form = 'раз'
+            if i[1][-1] == ['2', '3', '4']:
+                form = 'раза'
+            answer += '{} - {} {}, '.format(i[0], i[1], form)
 
     # remove comma at ending
     while not answer[-1].isalpha():
@@ -40,6 +47,6 @@ def counter(text):
     return answer
 
 if __name__ == '__main__':
-    # print(counter('./sample1.txt'))
+
     import doctest
     doctest.testmod()
