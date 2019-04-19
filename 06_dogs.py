9269097a38d8ee6ffc41c3d222e222c84222d4cf
Palevich <TBO9914@onetakeda.com>
2019-04-19 18:49:47 +0300

    Improved Dog.__add__() (still doesn't work)

--- 06_dogs.py
+++ 06_dogs.py
@@ -30,6 +30,7 @@ puppy.birth_date()
 from datetime import datetime
 from datetime import timedelta
 
+
 class Dog:
 
     def __init__(self, name, birth_date, voice):
@@ -38,37 +39,56 @@ class Dog:
         self.voice = voice
         self.birth_date = datetime(*birth_date)
 
-class Puppy(Dog):
+    def __add__(self, other):
+        elder_dog = self.birth_date
+        if self.birth_date > other.birth_date:
+            elder_dog = other.birth_date
 
-    def __init__(self, male, female):
+        # self.birth_date = elder_dog + timedelta(days=480)
+        # self.voice = '{voice1} {voice2}'.format(voice1=self.voice, voice2=other.voice)
+        # self.name = 'Щенок от собак {} и {}'.format(self.name, other.name)
 
-        self.dad_name = male.name
-        self.dad_voice = male.voice
-        self.dad_birth_date = male.birth_date
-
-        self.mom_name = female.name
-        self.mom_voice = female.voice
-        self.mom_birth_date = female.birth_date
+        birth_date = elder_dog + timedelta(days=480)
+        voice = '{voice1} {voice2}'.format(voice1=self.voice, voice2=other.voice)
+        name = 'Щенок от собак {} и {}'.format(self.name, other.name)
+        return Dog(name, birth_date, voice)
 
     def voice(self):
-        return '{voice1} {voice2}'.format(voice1=self.dad_voice, voice2=self.mom_voice)
+        return self.voice
 
     def name(self):
-        return 'Щенок от собак {} и {}'.format(self.dad_name, self.mom_name)
+        return self.name
 
     def birth_date(self):
-        elder_dog = self.dad_birth_date
+        return self.birth_date
 
-        if self.dad_birth_date < self.mom_birth_date:
-            elder_dog = self.mom_birth_date
+# class Puppy(Dog):
 
-        return elder_dog + timedelta(days=480)
+    # def __init__(self, male, female):
+    #
+    #     self.dad_name = male.name
+    #     self.dad_voice = male.voice
+    #     self.dad_birth_date = male.birth_date
+    #
+    #     self.mom_name = female.name
+    #     self.mom_voice = female.voice
+    #     self.mom_birth_date = female.birth_date
+
 
 boy_dog = Dog('Бобик', [2017, 1, 1], 'тяф-тяф')
 girl_dog = Dog('Тяпа', [2016, 1, 2], 'гав-гав')
-puppy = Puppy(boy_dog, girl_dog)
+puppy = boy_dog + girl_dog
 
 if __name__ == '__main__':
+
+    print(puppy.name())
+    print(puppy.voice())
+    print(puppy.birth_date())
+
+    print(puppy.name())
+    print(puppy.voice())
+    print(puppy.birth_date())
+
 
     print(puppy.name())
     print(puppy.voice())
