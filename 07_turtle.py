3458acef7f77a099615d997afb0c4a9a0c0cc2c2
Palevich <TBO9914@onetakeda.com>
2019-04-24 16:56:13 +0300

    Work out on commands parcer

--- 07_turtle.py
+++ 07_turtle.py
@@ -26,6 +26,7 @@ goto, 115, 70, 1, black
 goto, 150, 70, 1, black
 goto, 150, 40, 1, black
 endfill
+
 Каждое первое слово в новой строке этого файла соответствует методу из класса Turtle.
 А после запятой идут его параметры(если есть).
 
@@ -34,3 +35,26 @@ endfill
 >>>import turtle
 нужно прочитав команды из файла нарисовать движения "черепашки" на экране.
 """
+import turtle
+
+with open('./draw.txt', 'r') as draw_file:
+
+    for line in draw_file:
+        line_list = line.split(',')
+        if len(line_list) > 1:
+
+            if line_list[-1].isalpha():
+                exec(f'turtle.color("{line_list[-1]}")')
+
+            if beginfill in line_list[0]:
+                turtle.begin_fill()
+            if endfill in line_list[0]:
+                turtle.end_fill()
+            if circle in line_list:
+                exec('turtle.circle({line_list[1]},{line_list[2]})')
+            
+
+        else:
+            command = 'turtle.' + line_list[0]
+
+        exec(command)
