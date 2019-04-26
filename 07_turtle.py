aa1a885c9825721c81bbb617f03d8f48292000fb
Palevich <TBO9914@onetakeda.com>
2019-04-26 14:09:13 +0300

    Refactoring, readability improved

--- 07_turtle.py
+++ 07_turtle.py
@@ -1,67 +1,52 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-Поместить следующий набор команд текстовый файл draw.txt
-
-beginfill, black
-circle, 20, 1, black
-endfill
-penup
-goto, 120, 0, 1, black
-pendown
-beginfill, black
-circle, 20, 1, black
-endfill
-penup
-goto, 150, 40, 1,
-pendown
-beginfill, yellow
-goto, -30, 40, 1,black
-goto, -30, 70, 1,black
-goto, 60, 70, 1, black
-goto, 60, 100, 1, black
-goto, 90, 100, 1, black
-goto, 115, 70, 1, black
-goto, 150, 70, 1, black
-goto, 150, 40, 1, black
-endfill
-"""
-import turtle
+import turtle as t
 import time
 
 def create_drawing():
+
     with open('./draw.txt', 'r') as draw_file:
 
         for line in draw_file:
-            line_list = line.split(',')
 
-            for index, element in enumerate(line_list):
-                if element[-1].isdigit():
-                    line_list[index] = int(element)
+            line_list = line[:-1].split(',')
 
-            if len(line_list) > 1:
+            # Create commands and arguments
+            command = line_list[0]
+            if len(line_list) > 1 and line_list[1].strip().isdigit():
+                x = float(line_list[1])
+            if len(line_list) > 2 and line_list[2].strip().isdigit():
+                y = float(line_list[2])
+            if len(line_list) > 3 and line_list[3].strip().isdigit():
+                width = float(line_list[3])
+            if len(line_list) > 1 and line_list[-1].strip().isalpha():
+                color = line_list[-1].strip()
 
-                if line_list[-1].isalpha():
-                    exec(f'turtle.color("{line_list[-1]}")')
+            # catch a command and run it
+            if command == 'beginfill':
+                t.color(color)
+                t.begin_fill()
 
-                if 'beginfill' in line_list[0]:
-                    turtle.begin_fill()
-                if 'circle' in line_list:
-                    exec(f'turtle.circle({line_list[1]},{line_list[2]})')
-                if 'goto' in line_list[0]:
-                    turtle.pensize(int(line_list[-2]))
-                    turtle.goto(line_list[1], line_list[2])
+            if command == 'circle':
+                t.color(color)
+                t.circle(x, y)
 
-            else:
-                if 'pendown' in line_list[0]:
-                    turtle.pendown()
-                if 'penup' in line[0]:
-                    turtle.penup()
-                if 'endfill' in line_list[0]:
-                    turtle.end_fill()
+            if command == 'endfill':
+                t.end_fill()
 
-        time.sleep(10)
+            if command == 'penup':
+                t.penup()
+
+            if command == 'goto':
+                t.color(color)
+                t.width(width)
+
+            if command == 'pendown':
+                t.pendown()
+
+        time.sleep(3)
+
 
 
 if __name__ == '__main__':
