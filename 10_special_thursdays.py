2b54e10e19d178f14eab375a185105bb829fdb64
apalevich <artyompalevich@MacBook-Pro-Artyom.local>
2019-07-18 13:41:13 +0300

    Alternative func where months, week, weekday can be changed as arguments

--- 10_special_thursdays.py
+++ 10_special_thursdays.py
@@ -10,6 +10,11 @@ The return is a list containing datetime.date() objects:
 >>> special_thursdays(2019)
 [datetime.date(2019, 3, 21), datetime.date(2019, 6, 20), datetime.date(2019, 9, 19), datetime.date(2019, 12, 19)]
 
+Alternative function allow to change number of week and weeday by it's argument:
+
+>>> special_thursdays_alternative(year=2019, months=[3], week=2, weekday=2)
+[datetime.date(2019, 3, 13)]
+
 """
 
 import datetime
@@ -17,7 +22,7 @@ import datetime
 def special_thursdays(year: int) -> list:
     # Create list with required months and one for adding days
     months = [3, 6, 9, 12]
-    days_list = []
+    days_list = list()
 
     # Check weekdays in a loop until we find Thursday
     # starting from 15st day (1st day of 3th week)
@@ -29,6 +34,20 @@ def special_thursdays(year: int) -> list:
                 break
 
     return days_list
+
+def special_thursdays_alternative(year: int, months=[3, 6, 9, 12], week=3, weekday=3) -> list:
+    days_list = list()
+    min_day = (week-1) * 7 + 1
+
+    for month in months:
+        for day in range(min_day, min_day + 8):
+            current_date = datetime.date(int(year), month, day)
+            if current_date.weekday() == weekday:
+                days_list.append(current_date)
+                break
+
+    return days_list
+
 
 if __name__ == '__main__':
 
